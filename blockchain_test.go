adfa5892c60c87199e7b1e7858f7629db2312d20
vicent-dev <vicent@vicentdev.com>
2021-09-13 12:38:06 +0200

    test same blockchain same seeds

--- blockchain_test.go
+++ blockchain_test.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"github.com/matryer/is"
+	"log"
 	"os"
 	"strconv"
 	"testing"
 )
 
 const fileBlockchainTest = "blockchain_test.txt"
+const fileBlockchainOtherTest = "blockchain_test_1.txt"
 
 func TestCreateBlockchainToFile(t *testing.T) {
 	i := is.New(t)
@@ -22,7 +25,7 @@ func TestCreateBlockchainToFile(t *testing.T) {
 	os.Remove(fileBlockchainTest)
 }
 
-func TestRestoreBlochainFromFile(t *testing.T) {
+func TestRestoreBlockchainFromFile(t *testing.T) {
 	i := is.New(t)
 	b, err := NewBlockchain(fileBlockchainTest)
 	if err != nil {
@@ -37,6 +40,42 @@ func TestRestoreBlochainFromFile(t *testing.T) {
 	i.Equal(b.last.previous.data, bFromFile.last.previous.data)
 
 	os.Remove(fileBlockchainTest)
+}
+func TestSameHashSameSeedDifferentInstance(t *testing.T) {
+	i := is.New(t)
+	b, _ := NewBlockchain(fileBlockchainTest)
+	bo, _ := NewBlockchain(fileBlockchainOtherTest)
+
+	generateFakeDataBlockchain(b)
+	generateFakeDataBlockchain(bo)
+
+	f, err := os.OpenFile(fileBlockchainTest, os.O_RDONLY|os.O_CREATE, 0600)
+	fo, err := os.OpenFile(fileBlockchainOtherTest, os.O_RDONLY|os.O_CREATE, 0600)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer closeFile(f)
+	defer closeFile(fo)
+
+	scanner := bufio.NewScanner(f)
+
+	var l, lo []string
+	for scanner.Scan() {
+		l = append(l, scanner.Text())
+	}
+	scanner = bufio.NewScanner(fo)
+	for scanner.Scan() {
+		lo = append(lo, scanner.Text())
+	}
+
+	for it, _ := range l {
+		i.Equal(l[it], lo[it])
+	}
+
+	os.Remove(fileBlockchainTest)
+	os.Remove(fileBlockchainOtherTest)
 }
 
 func generateFakeDataBlockchain(b *Blockchain) string {
